bf8eeda487b4c3701af94ee2b25503e76c41a403
Shubham Gupta <32878682+IamShubhamGupto@users.noreply.github.com>
2020-04-15 23:42:29 +0530

    New test.py - read images metadata
    read geolocation using this code

--- PyPrograms/test.py
+++ PyPrograms/test.py
@@ -5,7 +5,7 @@ Created on Mon Apr 13 15:25:23 2020
 @author: shubham_2
 """
 from osgeo import gdal
-ds = gdal.Open(r"D:\SHUBHAM\CCBD\A2.tiff")
+ds = gdal.Open(r"D:\SHUBHAM\CCBD\QGIS\BLR\BLR.100.tif")
 width = ds.RasterXSize
 height = ds.RasterYSize
 gt = ds.GetGeoTransform()
@@ -14,3 +14,4 @@ miny = gt[3] + width*gt[4] + height*gt[5]
 maxx = gt[0] + width*gt[1] + height*gt[2]
 maxy = gt[3] 
 metadata = gdal.Info(ds)
+print(metadata)
