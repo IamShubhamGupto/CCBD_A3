b204ef9fe9748eafaa5ca56a02f45b6fb09c6638
Shubham Gupta <32878682+IamShubhamGupto@users.noreply.github.com>
2020-04-18 18:04:07 +0530

    New code to calc area
    old code gave area > 100%

--- PyPrograms/DetectGreen.py
+++ PyPrograms/DetectGreen.py
@@ -7,21 +7,26 @@ Created on Wed Apr 15 14:02:05 2020
 
 import cv2
 import numpy as np
-img = cv2.imread(r'D:\SHUBHAM\CCBD\tiles\blr_19_03.png')
+img = cv2.imread(r'D:\SHUBHAM\CCBD\QGIS\BLR\BLR.63.tif')
 h,w,c = img.shape
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_green = np.array([35,25,25])
 upper_green = np.array([80,255,255])
 mask = cv2.inRange(hsv, lower_green, upper_green)
-res = cv2.bitwise_and(img,img, mask=mask)
-cv2.imshow('res',res)
+cv2.imshow('mask',mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-ret,threshold = cv2.threshold(res,1,255,cv2.THRESH_BINARY)
-cv2.imshow('frame',threshold)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-white_pix = np.sum(threshold == 255)
-print("Number of white pixels = ",white_pix)
-area = white_pix/(h*w)
+#res = cv2.bitwise_and(img,img, mask=mask)
+#bi = cv2.
+#print(mask.shape)
+unique, counts = np.unique(mask, return_counts=True)
+d = dict(zip(unique, counts))
+print("d  = ",d)
+'''
+white_pixel = np.sum(mask == 255)
+black_pixel = np.sum(mask == 0)
+print("Number of white pixels = ",white_pixel)
+print("Number of black pixels = ",black_pixel)
+area = white_pixel/(white_pixel+black_pixel)
 print("Area of white pixels = ",area)
+'''
